d8f52d2bf30f0138c7f2bf5ccecbf3e25eba1169
Geneva Diaz <kukla1422@gmail.com>
2021-09-11 15:51:53 -0500

    added weather modal to index and script

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -18,3 +18,40 @@ function myFunction() {
     }
   }
 // $(document).foundation()
+
+// weather api 
+var current = document.getElementById('current');
+var forecast = document.getElementById('forecast');
+var fetchButton = $("#launchModal");
+var today = document.getElementById('today');
+var tomorrow = document.getElementById('tomorrow');
+function getApi() {
+  fetch("https://weatherapi-com.p.rapidapi.com/forecast.json?q='San Antonio'&days=1", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+		"x-rapidapi-key": "901cbaf60cmsh5e5bea7d5f1a8bap1edabajsn89697e7fb7b8"
+	}})
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+          today.innerHTML = "Today's weather for San Antonio is ";
+          tomorrow.innerHTML = "Tomorrow's weather will be ";
+          current.innerHTML = data.current.condition.text;
+          forecast.innerHTML = data.forecast.forecastday[0].day.condition.text;
+      })
+
+  };
+
+// check weather button cals weather api and modal function
+fetchButton.click(function() {
+  getApi();
+  $(".modal").addClass("is-active");
+});
+
+
+$(".modal-close").click(function() {
+   $(".modal").removeClass("is-active");
+});
